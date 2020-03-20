58bf0b112ef67c07d15f058d97fac2da93701dde
zenglingjiao <1224312417@qq.com>
2020-03-20 17:29:22 +0800

    默认路由修改

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,8 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-//$route['default_controller'] = 'back/Admin';
-$route['default_controller'] = 'Stage';
+$route['default_controller'] = 'back/Admin';
+// $route['default_controller'] = 'Stage';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
